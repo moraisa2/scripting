1ad5609026793b7dc6fc2257e8befe97f1dd1115
moraisa2 <61420957+moraisa2@users.noreply.github.com>
2020-09-10 11:06:05 +0100

    Update facet_markets.sh
    Added support for GLI and fix PRDSS uri.

--- facet_markets.sh
+++ facet_markets.sh
@@ -8,16 +8,17 @@ echo "[1] - prdss"
 echo "[2] - prdwv"
 echo "[3] - nxt"
 echo "[4] - intbs1"
+echo "[5] - glibs1"
 
 read -p "Your environment: " usr_in
 
-if [[ $usr_in -lt 0 || $usr_in -gt 4 ]]
+if [[ $usr_in -lt 0 || $usr_in -gt 5 ]]
 then
   echo "Not a valid environment"
   exit 1
 elif [ $usr_in -eq 1 ]
 then
-  uri="http://prdss-scanfd-prdss.prd.fndlsb.net/api/sports/navigation/facet/v1/search"
+  uri="http://njss1-scanfd-prdss.prd.fndlsb.net/api/sports/navigation/facet/v1/search"
 elif [ $usr_in -eq 2 ]
 then
   uri="http://prdwv-scanfd-prdwv.prd.fndlsb.net/api/sports/navigation/facet/v1/search"
@@ -27,12 +28,15 @@ then
 elif [ $usr_in -eq 4 ]
 then
   uri="http://njss1-scanfd-intbs1.dev.fndlsb.net/api/sports/navigation/facet/v1/search"
+elif [ $usr_in -eq 5 ]
+then
+  uri="http://njss1-scanfd-glibs1.dev.fndlsb.net/api/sports/navigation/facet/v1/search"
 fi
 
 echo "Querying $uri for string \"sportsbookMarketId\""
 echo "=================================================================="
 
-NUMBER_SB_MARKETS=$(curl --location --request POST $uri --header 'X-Application: dyMLAanpRyIsjkpJ' --header 'cache-control: no-cache' --header 'Content-Type: application/json' --header 'Cookie: ssoid=PLTBXSHGVFCU9ICM7NIUG8IC3ZWT95P4; ssoid=0AR3PII6XZO6L0FT338G8JK1YNTLBL41' --data-raw '{
+NUMBER_SB_MARKETS=$(curl --location --request POST $uri --header 'X-Application: dyMLAanpRyIsjkpJ' --header 'cache-control: no-cache' --header 'Content-Type: application/json' --data-raw '{
   "filter": {
   }
 }' | grep -o sportsbookMarketId | wc -l | tr -d ' ')
